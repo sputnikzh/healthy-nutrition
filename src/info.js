57d7c92d3b337b362a8faf58f771b2eea428ebc5
LikerK <nagorniykirill@gmail.com>
2025-05-21 19:56:54 +0400

    fix gender

--- src/info.js
+++ src/info.js
@@ -1,28 +1,29 @@
 import { getDatabase, ref, onValue, set } from "firebase/database";
-import vegetables from "./vegetables.png"
+import vegetables from "./vegetables.png";
+
 const getDataUser = (login) => {
+  let gender = "male";
   const div = document.createElement("div");
-  div.classList.add("infoDiv")
-  
+  div.classList.add("infoDiv");
+
   const height = document.createElement("input");
   const weight = document.createElement("input");
   const age = document.createElement("input");
   const row1 = document.createElement("div");
   const row2 = document.createElement("div");
-  const vegetablesImg = document.createElement("img")
-  const form = document.createElement("form")
-  form.classList.add("infoForm")
-  vegetablesImg.classList.add("vegetablesImg")
-  height.setAttribute("name", "height")
-  weight.setAttribute("name", "weight")
-  age.setAttribute("name", "age")
-  
-  height.setAttribute("type", "text")
-  weight.setAttribute("type", "text")
-  age.setAttribute("type", "text")
+  const vegetablesImg = document.createElement("img");
+  const form = document.createElement("form");
+  form.classList.add("infoForm");
+  vegetablesImg.classList.add("vegetablesImg");
+  height.setAttribute("name", "height");
+  weight.setAttribute("name", "weight");
+  age.setAttribute("name", "age");
 
+  height.setAttribute("type", "text");
+  weight.setAttribute("type", "text");
+  age.setAttribute("type", "text");
 
-  vegetablesImg.setAttribute("src", vegetables)
+  vegetablesImg.setAttribute("src", vegetables);
 
   const maleLabel = document.createElement("label");
   const femaleLabel = document.createElement("label");
@@ -41,14 +42,16 @@ const getDataUser = (login) => {
   femaleLabel.textContent = "Жен";
 
   row1.appendChild(maleLabel);
-  row1.classList.add("genderDiv")
+  row1.classList.add("genderDiv");
   row1.appendChild(maleInput);
-  row2.classList.add("genderDiv")
+  row2.classList.add("genderDiv");
 
   row2.appendChild(femaleLabel);
   row2.appendChild(femaleInput);
-  div.appendChild(vegetablesImg)
-  div.appendChild(form)
+  div.appendChild(vegetablesImg);
+  div.appendChild(form);
+
+  enterBTN.setAttribute("type", "submit");
 
   const heightText = document.createElement("label");
   heightText.textContent = "Введите ваш рост:";
@@ -77,13 +80,26 @@ const getDataUser = (login) => {
   //   set(ref(db, 'users/' + login), information);
   //   window.location.reload();
   // });
-  form.addEventListener("submit", (e)=>{
-    e.preventDefault()
-    const data = new FormData(form)
-    console.log(data)
 
-  })
+  maleInput.addEventListener("click", () => {
+    gender = "male";
+  });
+
+  femaleInput.addEventListener("click", () => {
+    gender = "female";
+  });
+
+  form.addEventListener("submit", (e) => {
+    const information = {};
+    information["height"] = height.value;
+    information["wight"] = weight.value;
+    information["age"] = age.value;
+    information["gender"] = gender;
+    console.log(information);
+    e.preventDefault();
+  });
 
   return div;
 };
+
 export default getDataUser;
