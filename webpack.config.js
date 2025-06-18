c1cea816a51619aa9e0163892361364e9fff9b33
LikerK <nagorniykirill@gmail.com>
2025-06-18 19:18:11 +0400

    fix vercer

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 import HtmlWebpackPlugin from 'html-webpack-plugin';
-import Dotenv from 'dotenv-webpack';
+import webpack from 'webpack';
 
 export default {
   mode: process.env.NODE_ENV || 'development',
@@ -26,7 +26,15 @@ export default {
     new HtmlWebpackPlugin({
       template: 'index.html',
     }),
-    new Dotenv(), // это всё, что нужно
+    new webpack.DefinePlugin({
+      'process.env.API_KEY': JSON.stringify(process.env.API_KEY),
+      'process.env.AUTH_DOMAIN': JSON.stringify(process.env.AUTH_DOMAIN),
+      'process.env.PROJECT_ID': JSON.stringify(process.env.PROJECT_ID),
+      'process.env.STORAGE_BICKET': JSON.stringify(process.env.STORAGE_BICKET),
+      'process.env.MESSAGING_SENDER_ID': JSON.stringify(process.env.MESSAGING_SENDER_ID),
+      'process.env.APP_ID': JSON.stringify(process.env.APP_ID),
+      'process.env.DATABASE_URL': JSON.stringify(process.env.DATABASE_URL),
+    }),
   ],
   output: {
     clean: true,
