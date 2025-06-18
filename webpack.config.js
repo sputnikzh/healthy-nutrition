f170c04080661ddb4ecd7fc544a5128df86ca0c2
LikerK <nagorniykirill@gmail.com>
2025-06-18 18:59:42 +0400

    add settings

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,16 @@ export default {
     new HtmlWebpackPlugin({
       template: 'index.html',
     }),
-    new Dotenv()
+    new Dotenv(),
+    new webpack.DefinePlugin({
+      'process.env.API_KEY': JSON.stringify(process.env.API_KEY),
+      'process.env.AUTH_DOMAIN': JSON.stringify(process.env.AUTH_DOMAIN),
+      'process.env.PROJECT_ID': JSON.stringify(process.env.PROJECT_ID),
+      'process.env.STORAGE_BICKET': JSON.stringify(process.env.STORAGE_BICKET),
+      'process.env.MESSAGING_SENDER_ID': JSON.stringify(process.env.MESSAGING_SENDER_ID),
+      'process.env.APP_ID': JSON.stringify(process.env.APP_ID),
+      'process.env.DATABASE_URL': JSON.stringify(process.env.DATABASE_URL),
+    }),
   ],
   output: {
     clean: true,
