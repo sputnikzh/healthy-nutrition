2e00836a99945ab729630098beedaece4c0bf4d3
LikerK <nagorniykirill@gmail.com>
2025-06-18 19:13:37 +0400

    fix error for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -26,16 +26,7 @@ export default {
     new HtmlWebpackPlugin({
       template: 'index.html',
     }),
-    new Dotenv(),
-    new webpack.DefinePlugin({
-      'process.env.API_KEY': JSON.stringify(process.env.API_KEY),
-      'process.env.AUTH_DOMAIN': JSON.stringify(process.env.AUTH_DOMAIN),
-      'process.env.PROJECT_ID': JSON.stringify(process.env.PROJECT_ID),
-      'process.env.STORAGE_BICKET': JSON.stringify(process.env.STORAGE_BICKET),
-      'process.env.MESSAGING_SENDER_ID': JSON.stringify(process.env.MESSAGING_SENDER_ID),
-      'process.env.APP_ID': JSON.stringify(process.env.APP_ID),
-      'process.env.DATABASE_URL': JSON.stringify(process.env.DATABASE_URL),
-    }),
+    new Dotenv(), // это всё, что нужно
   ],
   output: {
     clean: true,
